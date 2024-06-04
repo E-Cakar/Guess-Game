754f005cc8020c24934deba9b35f282919f15105
E-Cakar <emrecakar2008@gmail.com>
2024-06-04 22:10:23 +0300

    Add files via upload

--- GuessGame.cpp
+++ GuessGame.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-#include <unistd.h>
-main()
-{
-int max = 0;
-int whilecounter = 0;
-int answer = 0;
-int counter = 0;
-printf("Welcome to Guess The Number game. Give me a maximum number:");
-scanf("%d",&max);
-printf("\nNow think of a number lower than %d and i will try to find it!!\n", max);
-    int guess = max/2;
-    int a = 0;
-int b = max;
-    sleep(4);
-while (whilecounter == 0){
-printf("\nIs this your number? (1:Higher 2:Lower 3:Yes, it is!) %d\n", guess);
-        counter++;
-scanf("%d",&answer);
-            if (answer == 1){
-                a=guess;
-                guess = (b+guess)/2;
-            }
-            if (answer == 2){
-                b=guess;
-                guess = (a+guess)/2;
-            }
-            if (answer == 3){
-                whilecounter = 1;
-            }
+int input;
+int max;
+int whileCounter;
+int guess;
+int min;
+int max1;
+int main(){
+std::cout << "Lutfen Maksimum bir deger belirleyin ve bir sayi tutun, bu sayiyi tahmin etmeye calisacagim."<< std::endl;
+std::cin >> max;
+std::cout << "Maksimum degeriniz: "<< max <<"."<< std::endl;
+guess = max/2;
+max1 = max;
+std::cout <<"Tahminim: " << guess <<"."<<"Eger tuttugunuz sayi tahmin ettigim sayidan; Daha buyukse 1, Daha kucukse 2, Eger dogru bildiysem 3 yazin. ";
+while(whileCounter < 1){
+    std::cin >> input;
+    if (input == 1){
+        min = guess;
+        guess = guess + ((max1-guess)/2);
+        std::cout <<"Tahminim: " << guess <<"."<<"Eger tuttugunuz sayi tahmin ettigim sayidan; Daha buyukse 1, Daha kucukse 2, Eger dogru bildiysem 3 yazin. ";
+    }
+    if (input == 2){
+        max1 = guess;
+        guess = min + ((guess-min)/2);
+        std::cout <<"Tahminim: " << guess <<"."<<"Eger tuttugunuz sayi tahmin ettigim sayidan; Daha buyukse 1, Daha kucukse 2, Eger dogru bildiysem 3 yazin. ";
+    }
+    if (input == 3){
+        whileCounter += 5;
+    }
+    if (input == 5){
+        std::cout <<min<<std::endl << max1 << std::endl;
 }
-printf("YAY!! I found it in %d moves!! Thanks For Playing!", counter);
-return 0;
+}
 }

d3abf221e4183c998ddf1722992161074322c4b9
E-Cakar <emrecakar2008@gmail.com>
2024-06-04 22:15:40 +0300

    Add files via upload

--- GuessGame.cpp
+++ GuessGame.cpp
@@ -5,6 +5,7 @@ int whileCounter;
 int guess;
 int min;
 int max1;
+int counter;
 int main(){
 std::cout << "Lutfen Maksimum bir deger belirleyin ve bir sayi tutun, bu sayiyi tahmin etmeye calisacagim."<< std::endl;
 std::cin >> max;
@@ -15,20 +16,20 @@ std::cout <<"Tahminim: " << guess <<"."<<"Eger tuttugunuz sayi tahmin ettigim sa
 while(whileCounter < 1){
     std::cin >> input;
     if (input == 1){
+        counter ++;
         min = guess;
         guess = guess + ((max1-guess)/2);
         std::cout <<"Tahminim: " << guess <<"."<<"Eger tuttugunuz sayi tahmin ettigim sayidan; Daha buyukse 1, Daha kucukse 2, Eger dogru bildiysem 3 yazin. ";
     }
     if (input == 2){
+        counter ++;
         max1 = guess;
         guess = min + ((guess-min)/2);
         std::cout <<"Tahminim: " << guess <<"."<<"Eger tuttugunuz sayi tahmin ettigim sayidan; Daha buyukse 1, Daha kucukse 2, Eger dogru bildiysem 3 yazin. ";
     }
     if (input == 3){
+        std::cout << "Yeeey! "<< counter <<" Denemede bildim.";
         whileCounter += 5;
     }
-    if (input == 5){
-        std::cout <<min<<std::endl << max1 << std::endl;
-}
 }
 }
